a6453a9802b8054d6d70b87ebf3c500090dc26ff
m-sugawara <masa.sugawar@gmail.com>
2018-07-07 12:18:45 +0900

    Initial Commit

---
+++ SpeechRecognizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpeechRecognizer
+//
+//  Created by M_Sugawara on 2018/07/07.
+//  Copyright © 2018年 Sugawar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
